c58ed8ef0239b9392c181d94cf002447a6f256c6
Dmitrijs Sadovskis <sdo@inbox.lv>
2019-11-10 11:32:03 +0200

    Cleanup, fixes

--- app/renderer/Store/Store.ts
+++ app/renderer/Store/Store.ts
@@ -15,127 +15,6 @@ function saveTasks(state) {
 const store = new Vuex.Store({
     state: {
         tasks: null,
-        // fromJS({
-        // 1: Map({
-        //     code: 'TSKS-1111', title: 'Create task from some sentence and that is it', distributed: false, chargeable: false, logged: false,
-        //     time_spent_seconds: 3600 + 15 * 60 + 45,
-        //     date: '2019-10-16',
-        // }),
-        // 2: Map({
-        //     code: 'TSKS-3333', title: 'Blog creation', distributed: true, frozen: true, chargeable: false, logged: false,
-        //     time_spent_seconds: 12 * 3600 + 15 * 60 + 45,
-        //     date: '2019-10-16',
-        // }),
-        // 3: {
-        //     id: 3,
-        //     code: 'TSKS-5444',
-        //     title: 'Enjoy a rest ater this work is done to have some not at all exception there is not a thing for this',
-        //     distributed: true, frozen: false,
-        //     chargeable: false,
-        //     logged: false,
-        //     time_spent_seconds: 3 * 3600 + 54 * 60 + 45,
-        //     date: '2019-10-16',
-        // },
-        // 4: {
-        //     id: 4,
-        //     code: 'TSKS-1111', title: 'Create task from some sentence and that is it', distributed: false, chargeable: true, logged: false,
-        //     time_spent_seconds: 43 * 60 + 45,
-        //     date: '2019-10-14',
-        // },
-        // 5: {
-        //     id: 5,
-        //     code: 'TSKS-3333', title: 'Blog creation', distributed: true, frozen: false, chargeable: true, logged: false,
-        //     time_spent_seconds: 2 * 3600 + 2 * 60 + 45,
-        //     date: '2019-10-07',
-        // },
-        // 6: {
-        //     id: 6,
-        //     code: 'TSKS-5444',
-        //     title: 'Enjoy a rest ater this work is done to have some not at all exception there is not a thing for this',
-        //     distributed: true, frozen: true,
-        //     chargeable: true,
-        //     logged: false,
-        //     time_spent_seconds: 1 * 60 + 45,
-        //     date: '2019-10-10',
-        // },
-        // 7: {
-        //     id: 7,
-        //     code: 'TSKS-1243',
-        //     title: 'Add some tasks to some tasks for some tasks',
-        //     distributed: true,
-        //     frozen: false,
-        //     chargeable: true,
-        //     logged: false,
-        //     time_spent_seconds: 3 * 60 + 45,
-        //     date: '2019-10-10',
-        // },
-        // 8: {
-        //     id: 8,
-        //     code: 'TSKS-1111', title: 'Create task from some sentence and that is it', distributed: false, chargeable: true, logged: true,
-        //     time_spent_seconds: 3600 + 33 * 60 + 45,
-        //     date: '2019-10-10',
-        // },
-        // 9: {
-        //     id: 9,
-        //     code: 'TSKS-3333', title: 'Blog creation', distributed: true, frozen: true, chargeable: true, logged: false,
-        //     time_spent_seconds: 3600 + 15 * 60 + 45,
-        //     date: '2019-10-10',
-        // },
-        // 10: {
-        //     id: 10,
-        //     code: 'TSKS-5444',
-        //     title: 'Enjoy a rest ater this work is done to have some not at all exception there is not a thing for this',
-        //     distributed: true, frozen: true,
-        //     chargeable: true,
-        //     logged: true,
-        //     time_spent_seconds: 3600 + 15 * 60 + 45,
-        //     date: '2019-10-12',
-        // },
-        // 11: {
-        //     id: 11,
-        //     code: 'TSKS-1111', title: 'Create task from some sentence and that is it', distributed: false, chargeable: true, logged: true,
-        //     time_spent_seconds: 3600 + 15 * 60 + 45,
-        //     date: '2019-10-12',
-        // },
-        // 12: {
-        //     id: 12,
-        //     code: 'TSKS-3333', title: 'Blog creation', distributed: true, frozen: true, chargeable: true, logged: false,
-        //     time_spent_seconds: 3600 + 15 * 60 + 45,
-        //     date: '2019-10-12',
-        // },
-        // 13: {
-        //     id: 13,
-        //     code: 'TSKS-5444',
-        //     title: 'Enjoy a rest ater this work is done to have some not at all exception there is not a thing for this',
-        //     distributed: true, frozen: false,
-        //     chargeable: true,
-        //     logged: false,
-        //     time_spent_seconds: 4 * 3600 + 0 * 60 + 0,
-        //     date: '2019-10-16',
-        // },
-        // 14: {
-        //     id: 14,
-        //     code: 'TSKS-1111', title: 'Create task from some sentence and that is it', distributed: false, chargeable: true, logged: false,
-        //     time_spent_seconds: 3 * 3600 + 0 * 60 + 0,
-        //     date: '2019-10-16',
-        // },
-        // 15: {
-        //     id: 15,
-        //     code: 'TSKS-3333', title: 'Blog creation', distributed: true, frozen: false, chargeable: true, logged: false,
-        //     time_spent_seconds: 2 * 3600 + 0 * 60 + 0,
-        //     date: '2019-10-16',
-        // },
-        // 16: {
-        //     id: 16,
-        //     code: 'TSKS-5444',
-        //     title: 'Enjoy a rest ater this work is done to have some not at all exception there is not a thing for this',
-        //     distributed: true, frozen: true,
-        //     chargeable: true,
-        //     logged: false,
-        //     time_spent_seconds: 3600 + 0 * 60 + 0,
-        //     date: '2019-10-16',
-        // },
-        // }),
 
         tasksSelectedIds: Map<Number, Boolean>({}),
         tasksHoveredId: null,
@@ -180,15 +59,16 @@ const store = new Vuex.Store({
                 let not_distributed = 0;
 
                 tasks.forEach((task) => {
-                    spent += (task.time_spent_seconds);
+                    const seconds = parseInt(task.time_spent_seconds);
+                    spent += (seconds);
                     if (task.chargeable) {
-                        charge += task.time_spent_seconds;
+                        charge += seconds;
 
                         if (task.distributed) {
-                            distributed += task.time_spent_seconds;
+                            distributed += seconds;
                         } else {
                             if (!task.frozen) {
-                                not_distributed += task.time_spent_seconds;
+                                not_distributed += seconds;
                             }
                         }
                     }
@@ -225,7 +105,7 @@ const store = new Vuex.Store({
                         task.time_charge_seconds = task.time_spent_seconds;
 
                     } else {
-                        let spent = task.time_spent_seconds;
+                        let spent = parseInt(task.time_spent_seconds);
 
                         task.time_charge_seconds = spent + ((spent / not_distributed) * distributed);
                     }
