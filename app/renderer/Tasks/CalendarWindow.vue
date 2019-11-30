ad96624c6327e3c030c56c3a71e980f49406722e
Dmitrijs Sadovskis <sdo@inbox.lv>
2019-11-30 15:30:27 +0200

    Updated calendar view

--- app/renderer/Tasks/CalendarWindow.vue
+++ app/renderer/Tasks/CalendarWindow.vue
@@ -1,10 +1,14 @@
 <template>
     <div class="CalendarWindow">
-        <button
-                v-for="file of allFiles"
-                @click="open(file)">
-            {{ file }}
-        </button>
+        <br/>
+        <template v-for="(days) of filesByWeeks">
+            <template v-for="(day) of days">
+                <button @click="open(day.fn)" :class="{ Weekend: day.isWeekend }">
+                    <strong>{{ day.title }}</strong> {{day.suffix}}
+                </button>&nbsp;
+            </template>
+            <hr/>
+        </template>
     </div>
 </template>
 
@@ -12,23 +16,53 @@
     import Vue from "vue";
     import Component from "vue-class-component";
     import store from "../Store/Store";
+    import {List} from "immutable";
+    import {comparatorGt} from "../Utils/Utils";
+
+    const moment = require("moment");
 
     @Component({})
     export default class CalendarWindow extends Vue {
-        data() {
-            return {}
-        }
+        get filesByWeeks() {
+            let fileNames = store.getters.getAllFiles;
 
-        get allFiles() {
-            return store.getters.getAllFiles;
+            let list = List();
+
+            for (let fn of fileNames) {
+                let day = moment(fn, 'YYYY-MM-DD');
+                let dowIso = parseInt(day.format("E"));
+
+                let suffix = "";
+                if (dowIso === 1 || dowIso === 5) {
+                    suffix = day.format("(MMM D)");
+                }
+                list = list.push({
+                    fn: fn,
+                    week: parseInt(day.format("W")),
+                    title: day.format('ddd'),
+                    suffix: suffix,
+                    isWeekend: (dowIso >= 6),
+                    counter: dowIso,
+                });
+            }
+
+            let map = list.groupBy((x) => x['week']);
+            map = map.map((list) => {
+                return list.sortBy((val) => val['counter'], comparatorGt);
+            });
+            map = map.toOrderedMap().sortBy((val, key) => -key);
+            return map.toList();
         }
 
         open(day) {
             store.commit.setDay(day);
-            store.commit.setScreen('tasks');
+            store.commit.returnToTasksScreen();
         }
     }
 </script>
 
 <style scoped lang="scss">
+    .Weekend {
+        opacity: 0.3;
+    }
 </style>
