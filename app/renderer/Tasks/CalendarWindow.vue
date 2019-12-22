973a5c00fa01c839c0b51f3c2418e481ff794f2f
Dmitrijs Sadovskis <sdo@inbox.lv>
2019-12-22 11:05:49 +0200

    Show time charged in the calendar

--- app/renderer/Tasks/CalendarWindow.vue
+++ app/renderer/Tasks/CalendarWindow.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="CalendarWindow">
         <br/>
+        <div>
+            <div v-for="(week, week_key) of weeks" class="Week">
+                <h4>{{ week_key }}</h4>
+                <span v-for="(day) of week" class="Day">
+                    <button @click="open(day.dayCode)">
+                        <strong>{{ day }}</strong>
+                    </button>&nbsp;
+                </span>
+            </div>
+        </div>
+
         <template v-for="(days) of filesByWeeks">
             <template v-for="(day) of days">
                 <button @click="open(day.fn)" :class="{ Weekend: day.isWeekend }">
@@ -17,18 +28,58 @@
     import Component from "vue-class-component";
     import store from "../Store/Store";
     import {List} from "immutable";
-    import {comparatorGt} from "../Utils/Utils";
+    import {comparatorGt, timespanToText} from "../Utils/Utils";
 
     const moment = require("moment");
 
     @Component({})
     export default class CalendarWindow extends Vue {
+        get weeks() {
+            let weeks = {};
+            let today = moment.utc();
+            let endOfThisMonth = moment(today).endOf('month').endOf('day');
+            let startOfMonthBeforePrevious = moment(today).subtract(2, 'months').startOf('month');
+
+            let totals = store.getters.getFileTotals;
+
+            let day = moment(startOfMonthBeforePrevious);
+            do {
+                // make sure it's NEXT_YEAR-01 on December 31
+                let weekCode = moment(day).endOf('isoWeek').format('YYYY-WW');
+                let week = weeks[weekCode] || {};
+                let dayCode = day.format('YYYY-MM-DD');
+
+                {
+                    let dowIso = parseInt(day.format("E"));
+
+                    let suffix = "";
+                    if (dowIso === 1 || dowIso === 5) {
+                        suffix = day.format("(MMM D)");
+                    }
+                    this.$set(week, dayCode, {
+                        dayCode: dayCode,
+                        title: day.format('DD'),
+                        // suffix: suffix,
+                        // isWeekend: (dowIso >= 6),
+                        // counter: dowIso,
+                        charged_seconds_text: timespanToText(totals[dayCode] ? totals[dayCode].time_charge_rounded_seconds : 0),
+                    });
+                }
+                this.$set(weeks, weekCode, week);
+
+                day.add(1, 'day');
+            } while (day.isBefore(endOfThisMonth));
+
+            return weeks;
+        }
+
         get filesByWeeks() {
             let fileNames = store.getters.getAllFiles;
 
             let list = List();
 
-            for (let fn of fileNames) {
+            for (let obj of fileNames) {
+                let fn = obj.key;
                 let day = moment(fn, 'YYYY-MM-DD');
                 let dowIso = parseInt(day.format("E"));
 
@@ -64,5 +115,10 @@
 <style scoped lang="scss">
     .Weekend {
         opacity: 0.3;
+    }
+
+    .CalendarWindow {
+        overflow-y: scroll;
+        max-height: 100%;
     }
 </style>
