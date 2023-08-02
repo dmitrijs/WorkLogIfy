7060e58f9f429f54301278fd43a20989746fff5e
Dmitrijs Sadovskis <sdo@inbox.lv>
2023-08-02 10:37:40 +0300

    patch(osx): disabled active app tracking on mac (permissions issue)

--- app/main/idle.ts
+++ app/main/idle.ts
@@ -9,20 +9,24 @@ export default class IdleUser {
     private static isOffline = false;
 
     public static registerOnReady(mainWindow) {
-        setInterval(() => {
-            const secondsIdle = electron.powerMonitor.getSystemIdleTime();
-            (async () => {
-                if (secondsIdle > this.seconds_to_log_active_window) {
-                    mainWindow.webContents.send('user-active-app', '-"-');
-                    return;
-                }
-                let obj = await activeWindow();
-                if (!obj) {
-                    return;
-                }
-                mainWindow.webContents.send('user-active-app', `[${obj.owner?.name || obj.owner?.path || obj.title}] "${obj.title}"`);
-            })();
-        }, this.seconds_to_log_active_window * 1000);
+        const isMac = process.platform === 'darwin'
+
+        if (!isMac) {
+            setInterval(() => {
+                const secondsIdle = electron.powerMonitor.getSystemIdleTime();
+                (async () => {
+                    if (secondsIdle > this.seconds_to_log_active_window) {
+                        mainWindow.webContents.send('user-active-app', '-"-');
+                        return;
+                    }
+                    let obj = await activeWindow();
+                    if (!obj) {
+                        return;
+                    }
+                    mainWindow.webContents.send('user-active-app', `[${obj.owner?.name || obj.owner?.path || obj.title}] "${obj.title}"`);
+                })();
+            }, this.seconds_to_log_active_window * 1000);
+        }
 
         setInterval(() => {
             const time = electron.powerMonitor.getSystemIdleTime();
