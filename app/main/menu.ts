3f7a3c7558cf3118b8a6c6aecf568a9b89d46caa
Dmitrijs Sadovskis <sdo@inbox.lv>
2023-08-14 18:10:06 +0300

    patch(tasks): enable copy&paste in text areas

--- app/main/menu.ts
+++ app/main/menu.ts
@@ -53,6 +53,18 @@ export default function createMainMenu(mainWindow) {
             label: 'WorkLogIfy',
             submenu: menuItems,
         }] : menuItems),
+        ...(isMac && [{
+            label: "Edit",
+            submenu: [
+                {label: "Undo", accelerator: "CmdOrCtrl+Z", role: "undo"},
+                {label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", role: "redo"},
+                {type: "separator"},
+                {label: "Cut", accelerator: "CmdOrCtrl+X", role: "cut"},
+                {label: "Copy", accelerator: "CmdOrCtrl+C", role: "copy"},
+                {label: "Paste", accelerator: "CmdOrCtrl+V", role: "paste"},
+                {label: "Select All", accelerator: "CmdOrCtrl+A", role: "selectAll"}
+            ],
+        } as any]),
         {
             label: '...',
             submenu: [
